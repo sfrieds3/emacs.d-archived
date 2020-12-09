87b8d104caebddeb5fe1bf47b47b73699f7c39fc
scwfri <scwfri@gmail.com>
2020-12-08 22:54:03 -0600

    undo stuff

--- lisp/scwfri/evil-config.el
+++ lisp/scwfri/evil-config.el
@@ -1,8 +1,10 @@
 (require 'evil)
 (evil-mode 1)
-(require 'undo-tree)
-(global-undo-tree-mode)
-(setq evil-want-fine-undo 'fine)
+
+(require 'undo-fu)
+
+(require 'undo-fu-session)
+(global-undo-fu-session-mode)
 
 ;; evil bindings for occur mode
 (add-hook 'occur-mode-hook
@@ -22,12 +24,13 @@
     (setq-default evil-symbol-word-search t)
     ;; Make horizontal movement cross lines
     (setq-default evil-cross-lines t)
+    (define-key evil-normal-state-map "u" 'undo-fu-only-undo)
+    (define-key evil-normal-state-map "\C-r" 'undo-fu-only-redo)
     (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
     (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
     (define-key evil-insert-state-map (kbd "C-u")
       (lambda ()
         (interactive)
-
-
+        (evil-delete (point-at-bol) (point))))))
 
 (provide 'evil-config)

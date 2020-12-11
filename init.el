e78e6806258ae274acbbe8738522c29db479517d
scwfri <1473181+scwfri@users.noreply.github.com>
2020-12-11 09:40:57 -0600

    Update init.el

--- init.el
+++ init.el
@@ -5,20 +5,19 @@
 ;;;     in .emacs.d directory to overwrite settings (loaded at end)
 
 ;;; Code:
-;;;; GENERAL PACKAGE SETTINGS
 
 (let ((default-directory  (expand-file-name "lisp" user-emacs-directory)))
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
 
-;; backup settings
+;;; backup settings
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 (setq delete-old-versions -1)
 (setq version-control t)
 (setq vc-make-backup-files t)
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list" t)))
 
-;; history settings
+;;; history settings
 (setq savehist-file (expand-file-name "savehist" user-emacs-directory))
 (savehist-mode 1)
 (setq history-length t)
@@ -33,16 +32,20 @@
   (when (file-exists-p home-settings)
     (load-file home-settings)))
 
-;; byte recompile everything
-;;(byte-recompile-directory (expand-file-name "~/.emacs.d/lisp") 0)
+;;; default 
+(setf find-def-directory command-line-default-directory)
+(add-hook 'find-file-hook
+ (lambda ()
+  (setq default-directory find-def-directory)
+  (setq find-name-arg "-iname")))
 
-;; ////////////////////////////////////////////////////////////
 
-;;;; THEME SETTINGS
+;;; byte recompile everything
+;;;(byte-recompile-directory (expand-file-name "~/.emacs.d/lisp") 0)
 
-;; ////////////////////////////////////////////////////////////
+;;; theme settings
 
-;; set default preferred fonts
+;;; set default preferred fonts
 (defvar platform-default-font)
 (setq platform-default-font
       (cond ((eq system-type 'windows-nt) "DejaVu Sans Mono 10")
@@ -56,18 +59,11 @@
 (load-theme 'ample t)
 (enable-theme 'ample)
 
-;; ////////////////////////////////////////////////////////////
-
-;;;; STARTUP SETTINGS
-
-;; ////////////////////////////////////////////////////////////
-
-;; start emacsclient if server not running already
-
+;;; start emacsclient if server not running already
 (load "server")
 (unless (server-running-p) (server-start))
 
-;; make scrolling work like it should
+;;; make scrolling work like it should
 (setq scroll-step 1)
 (setq scroll-conservatively 10000)
 (setq auto-window-vscroll nil)
@@ -76,30 +72,30 @@
 (setq mouse-wheel-follow-mouse 't)
 
 ;; cursor always blinks
-(setq blink-cursor-blinks -1)
+(;setq blink-cursor-blinks -1)
 
-;; visuals
+;;; visuals
 (setq inhibit-startup-screen t)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 
-;; inhibit visual bells
+;;; inhibit visual bells
 (setq visible-bell nil
       ring-bell-function #'ignore)
 
-;; highlight current line
+;;; highlight current line
 (global-hl-line-mode -1)
 (defvar hl-line-face)
 (set-face-attribute hl-line-face nil :underline nil)
 
-;; spaces by default instead of tabs!
+;;; spaces by default instead of tabs!
 (setq-default indent-tabs-mode nil)
 
-;; show matching parens
+;;; show matching parens
 (show-paren-mode 1)
 
-;; smart parens
+;;; smart parens
 (electric-pair-mode -1)
 ;; dont use smart parens in mini-buffers
 (defun my-inhibit-electric-pair-mode (char)
@@ -107,36 +103,31 @@
 
 (setq electric-pair-inhibit-predicate #'my-inhibit-electric-pair-mode)
 
-;; turn on recent file mode
+;;; turn on recent file mode
 (recentf-mode t)
 (setq recentf-max-saved-items 50)
 
-;; filename in titlebar
+;;; filename in titlebar
 (setq frame-title-format
       (concat "%f@" system-name))
 
-;; C-x w h [REGEX] <RET> <RET> to highlight all occurances of [REGEX], and C-x w r [REGEX] <RET> to unhighlight them again.
+;;; C-x w h [REGEX] <RET> <RET> to highlight all occurances of [REGEX], and C-x w r [REGEX] <RET> to unhighlight them again.
 (global-hi-lock-mode 1)
 
-;; stop asking about upcase and downcase region
+;;; stop asking about upcase and downcase region
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
-;; advanced management of rectangles
+;;; advanced management of rectangles
 ;; C-RET to mark a corner
 ;; M-r to do revexp replace within marked rectange
 ;; C-RET to unmark/exit rectangle editing
 (cua-selection-mode 1)
 
-;; transient mark mode
+;;; transient mark mode
 (transient-mark-mode 1)
-
-;; ////////////////////////////////////////////////////////////
-
-;; LOAD INIT FILES
-
-;; ////////////////////////////////////////////////////////////
-
+ 
+;;; LOAD INIT FILES
 (require 'shared-config)
 (require 'defun)
 (require 'evil-config)
@@ -147,16 +138,11 @@
 (require 'uniquify-config)
 (require 'elpy-config)
 
-;; ////////////////////////////////////////////////////////////
-
 ;; PACKAGES
 
-;; ////////////////////////////////////////////////////////////
 
 (require 'which-key)
 (which-key-mode)
-
-
 
 ;;;; SLIME
 (require 'slime-autoloads)
@@ -170,13 +156,8 @@
 (add-hook 'slime-mode-hook #'my/slime-keybindings)
 (add-hook 'slime-repl-mode-hook #'my/slime-keybindings)
 
-;; ////////////////////////////////////////////////////////////
-
-;;;; LANGUAGE SETTINGS AND PACKAGES
-
-;; ////////////////////////////////////////////////////////////
-
-;; c++
+;;; LANGUAGE SETTINGS
+;;; c++
 (defun my/c++-mode-hook ()
   "C++ mode stuff."
   (defvar c-basic-offset)
@@ -184,22 +165,17 @@
   (c-set-offset 'substatement-open 0))
 (add-hook 'c++-mode-hook 'my/c++-mode-hook)
 
-;; eldoc mode
+;;; eldoc mode
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'eldoc-mode)
 (add-hook 'ielm-mode-hook 'eldoc-mode)
-
-;; ////////////////////////////////////////////////////////////
-
-;;;; GENERAL KEY REMAPPINGS
-
-;; ////////////////////////////////////////////////////////////
-
-;; move between windows with shift-arrow keys
+ 
+;;; KEY REMAPPINGS
+;;; move between windows with shift-arrow keys
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
-;; custom functions
+;;; custom functions
 (global-set-key (kbd "C-c s d") 'my/dir-grep)
 (global-set-key (kbd "C-c s s") 'my/file-grep)
 (global-set-key (kbd "C-c s f") 'find-dired)
@@ -208,7 +184,7 @@
 (global-set-key (kbd "C-<tab>") 'my/goto-match-paren)
 (global-set-key (kbd "%") 'my/goto-match-paren)
 
-;; SPC commands
+;;; SPC commands
 (global-set-key (kbd "C-c SPC l") 'my/select-line)
 (global-set-key (kbd "C-c SPC r") 'replace-regexp)
 (global-set-key (kbd "C-c SPC i") 'indent-region)
@@ -217,7 +193,7 @@
 (global-set-key (kbd "C-c SPC b e") 'eval-buffer)
 (global-set-key (kbd "C-c SPC b r") 'revert-buffer)
 
-;; general customizations
+;;; general customizations
 (global-set-key (kbd "C-c [") 'previous-error)
 (global-set-key (kbd "C-c ]") 'next-error)
 (global-set-key (kbd "C-c o") 'occur)
@@ -226,7 +202,7 @@
 (global-set-key (kbd "C-c r") 'recentf-open-files)
 (global-set-key (kbd "C-x C-b") 'buffer-menu-other-window)
 
-;; window management
+;;; window management
 (global-set-key (kbd "C-S-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "C-S-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "C-S-<down>") 'shrink-window)
@@ -234,37 +210,30 @@
 (global-set-key (kbd "C-c D") 'delete-window)
 (global-set-key (kbd "M-o") 'other-window)
 
-;; hippie expand -- also C-M-i for completion mode
+;;; hippie expand -- also C-M-i for completion mode
 (global-set-key (kbd "C-.") 'hippie-expand)
 
-;; string insert region
+;;; string insert region
 (global-set-key (kbd "C-c I") 'string-insert-rectangle)
 
-;; indent
+;;; indent
 (global-set-key (kbd "C-x TAB") 'indent-code-rigidly)
 (global-set-key (kbd "C-M-<backspace>") 'my/kill-back-to-indent)
 
-;; want to go to correct indentation on enter
+;;; want to go to correct indentation on enter
 (global-set-key (kbd "RET") 'newline-and-indent)
 
-;; no C-z
+;;; no C-z
 (global-set-key (kbd "C-z") nil)
 
-;; version control
+;;; version control
 (global-set-key (kbd "C-c g \\") 'vc-diff)
 (global-set-key (kbd "C-c g h") 'vc-region-history)
 (global-set-key (kbd "C-c g s") 'vc-dir)
 
-;; ////////////////////////////////////////////////////////////
-
-;; platform specific files
-
-;; ////////////////////////////////////////////////////////////
-
+;;; LOAD LOCAL SETTINGS
 (let ((local-settings (expand-file-name "local-settings.el" user-emacs-directory)))
   (when (file-exists-p local-settings)
     (load-file local-settings)))
-
-;; ////////////////////////////////////////////////////////////
 
 (provide 'init.el)

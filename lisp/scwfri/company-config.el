82005b20eca3ca87dcdf116157fa1acbe969a036
scwfri <1473181+scwfri@users.noreply.github.com>
2020-12-11 09:41:47 -0600

    Update company-config.el

--- lisp/scwfri/company-config.el
+++ lisp/scwfri/company-config.el
@@ -1,6 +1,14 @@
+;;;; package --- summary
+
+;;; Commentary:
+;;;     company-config
+
+;;; Code:
+
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 (define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
 (define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort)
 
 (provide 'company-config)
+;;; company-config.el ends here

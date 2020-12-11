84cf538e041166a2b391e531a9c5915f99210652
scwfri <1473181+scwfri@users.noreply.github.com>
2020-12-11 09:43:43 -0600

    Update elpy-config.el

--- lisp/scwfri/elpy-config.el
+++ lisp/scwfri/elpy-config.el
@@ -1,9 +1,18 @@
-(load "elpy")
-(load "elpy-rpc")
-(load "elpy-shell")
-(load "elpy-profile")
-(load "elpy-refactor")
-(load "elpy-django")
-(elpy-enable)
+;;;; package --- summary
+
+;;; Commentary:
+;;;     elpy-config
+
+;;; Code:
+
+(if version< "24.3"
+  (load "elpy")
+  (load "elpy-rpc")
+  (load "elpy-shell")
+  (load "elpy-profile")
+  (load "elpy-refactor")
+  (load "elpy-django")
+  (elpy-enable))
 
 (provide 'elpy-config)
+;;; elpy-config ends here

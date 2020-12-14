615eaf36967dbfd0a5c97c052619dc5d37efcd7f
scwfri <scwfri@gmail.com>
2020-12-13 20:09:19 -0600

    update config

--- lisp/scwfri/defun.el
+++ lisp/scwfri/defun.el
@@ -8,19 +8,18 @@
 ;; mode line
 (setq-default mode-line-format
               (list
-                ;; file status info
-                mode-line-mule-info
-                mode-line-modified
-                mode-line-frame-identification
-                ;; current buffer name
-                "%b |"
-                ;; current git branch
-                '(vc-mode vc-mode)
-                ;; mode-name
-                " [%m] "
-                ;; current line and column number
-                "(%l:%c %P)"
-                ))
+               ;; file status info
+               mode-line-mule-info
+               mode-line-modified
+               mode-line-frame-identification
+               ;; current buffer name
+               "%b"
+               ;; current git branch
+               ;;'(vc-mode vc-mode)
+               ;; mode-name
+               " [%m] "
+               ;; current line and column number
+               "(%l:%c %P)"))
 
 ;; grep in current directory
 (defun my/dir-grep ()

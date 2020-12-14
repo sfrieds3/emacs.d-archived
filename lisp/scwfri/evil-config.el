311b067f77c940a1e0b435c62ed792efc8ed6969
scwfri <1473181+scwfri@users.noreply.github.com>
2020-12-14 15:25:36 -0600

    Update evil-config.el

--- lisp/scwfri/evil-config.el
+++ lisp/scwfri/evil-config.el
@@ -32,6 +32,7 @@
     (define-key evil-normal-state-map (kbd "\\w") 'delete-trailing-whitespace)
     (define-key evil-normal-state-map (kbd "\\f") 'find-name-dired)
     (define-key evil-normal-state-map (kbd "\\b") 'buffer-menu)
+    (define-key evil-normal-state-map (kbd "DEL") 'evil-switch-to-windows-last-buffer)
     (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
     (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
     (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)

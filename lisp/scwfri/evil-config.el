fa32b8d8a55a9c115386e8e665af995714b64639
scwfri <1473181+scwfri@users.noreply.github.com>
2020-12-14 14:27:29 -0600

    Update evil-config.el

--- lisp/scwfri/evil-config.el
+++ lisp/scwfri/evil-config.el
@@ -26,18 +26,24 @@
     (setq-default evil-symbol-word-search t)
     ;; Make horizontal movement cross lines
     (setq-default evil-cross-lines t)
-    (define-key evil-normal-state-map "u" 'my/simple-undo)
-    (define-key evil-normal-state-map "\C-r" 'my/simple-redo)
-    (define-key evil-normal-state-map "\\w" 'delete-trailing-whitespace)
-    (define-key evil-normal-state-map "\\RET" 'lazy-highlight-cleanup)
-    (define-key evil-normal-state-map "\\f" 'find-name-dired)
-    (define-key evil-normal-state-map "\\b" 'buffer-menu)
+    (define-key evil-normal-state-map (kbd "u") 'my/simple-undo)
+    (define-key evil-normal-state-map (kbd "C-r") 'my/simple-redo)
+    (define-key evil-normal-state-map (kbd "C-l") 'lazy-highlight-cleanup)
+    (define-key evil-normal-state-map (kbd "\\w") 'delete-trailing-whitespace)
+    (define-key evil-normal-state-map (kbd "\\f") 'find-name-dired)
+    (define-key evil-normal-state-map (kbd "\\b") 'buffer-menu        
+    (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
+    (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
     (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
     (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
     (define-key evil-insert-state-map (kbd "C-u")
       (lambda ()
         (interactive)
-        (evil-delete (point-at-bol) (point)))))) 
+        (evil-delete (point-at-bol) (point))))
+    (define-key evil-normal-state-map (kbd "_") 'universal-argument)
+    (define-key universal-argument-map (kbd "_") 'universal-argument-more)
+    (define-key universal-argument-map (kbd "C-u") nil)))
+ 
 
 (provide 'evil-config)
 ;;; evil-config.el ends here

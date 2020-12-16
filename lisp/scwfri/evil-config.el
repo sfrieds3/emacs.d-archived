8e3914c469ec9a596cf793e119fbe1b479b2d6d4
scwfri <1473181+scwfri@users.noreply.github.com>
2020-12-16 11:46:11 -0600

    Update evil-config.el

--- lisp/scwfri/evil-config.el
+++ lisp/scwfri/evil-config.el
@@ -9,6 +9,30 @@
 (require 'evil-numbers)
 (evil-mode 1)
 
+(defun my/clear-highlights ()
+  "Clear highlights from evil-search"
+  (interactive)
+  (evil-ex "nohl")
+  (exit-minibuffer))
+
+(defun my/star-keep-position ()
+  (interactive)
+  (case evil-search-module
+    (evil-search (progn
+                  (evil-ex-search-word-forward)
+                  (evil-ex-search-previous)))
+    (isearch (progn
+               (evil-search-word-forward)
+               (evil-search-previous)))))
+
+(defun my/visualstar-keep-position ()
+  (interactive)
+  (when (region-active-p)
+    (evil-visualstar/begin-search (region-beginning) (region-end) t)
+    (case evil-search-module
+      (evil-search (evil-ex-search-previous))
+      (isearch (evil-search-previous)))))
+
 ;; evil bindings for occur mode
 (add-hook 'occur-mode-hook
           (lambda ()
@@ -27,11 +51,12 @@
     (setq-default evil-symbol-word-search t)
     ;; Make horizontal movement cross lines
     (setq-default evil-cross-lines t)
+    (evil-select-search-module 'evil-search-module 'evil-search)
     (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
     (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
     (define-key evil-normal-state-map (kbd "u") 'my/simple-undo)
     (define-key evil-normal-state-map (kbd "C-r") 'my/simple-redo)
-    (define-key evil-normal-state-map (kbd "C-l") 'lazy-highlight-cleanup)
+    (define-key evil-normal-state-map (kbd "C-l") 'evil-ex-nohighlight)
     (define-key evil-normal-state-map (kbd "\\w") 'delete-trailing-whitespace)
     (define-key evil-normal-state-map (kbd "\\f") 'find-name-dired)
     (define-key evil-normal-state-map (kbd "\\b") 'buffer-menu)
@@ -39,14 +64,17 @@
     (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
     (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
     (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+    (define-key evil-normal-state-map (kbd "*") 'my/star-keep-position)
     (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
-    (define-key evil-visual-state-map "j" 'evil-next-visual-line)
-    (define-key evil-visual-state-map "k" 'evil-previous-visual-line)
+    (define-key evil-visual-state-map (kbd "j") 'evil-next-visual-line)
+    (define-key evil-visual-state-map (kbd "k") 'evil-previous-visual-line)
+    (define-key evil-visual-state-map (kbd "*") 'my/star-keep-position)
     (define-key evil-insert-state-map (kbd "C-u")
       (lambda ()
         (interactive)
         (evil-delete (point-at-bol) (point))))
     (define-key evil-normal-state-map (kbd "_") 'universal-argument)
+    ;(define-key evil-ex-map "e" 'find-file)
     (define-key universal-argument-map (kbd "_") 'universal-argument-more)
     (define-key universal-argument-map (kbd "C-u") nil)))
  

f47486f79f13099d0c3b852f2f7bbb51b896dcf8
scwfri <1473181+scwfri@users.noreply.github.com>
2020-12-21 13:54:49 -0600

    Update evil-config.el

--- lisp/scwfri/evil-config.el
+++ lisp/scwfri/evil-config.el
@@ -14,19 +14,19 @@
 (evil-collection-init)
 (global-evil-visualstar-mode)
 
-(defun $clear-highlights ()
+(defun $evil-clear-highlights ()
   "Clear highlight from evil-search."
   (interactive)
   (evil-ex "nohl")
   (exit-minibuffer))
 
-(defun $star-keep-position ()
+(defun $evil-star-keep-position ()
   "Keep position when searching."
   (interactive)
   (evil-search-word-forward)
   (evil-search-previous))
 
-(defun $visualstar-keep-position ()
+(defun $evil-visualstar-keep-position ()
   "Keep current position on visual star search."
   (interactive)
   (when (region-active-p)
@@ -39,11 +39,21 @@
   (evil-set-jump))
 (advice-add 'dumb-jump-goto-file-line :before #'$evil-set-jump-args)
 
-(defun $split-right-and-move ()
+(defun $evil-split-right-and-move ()
   "Split window to the right and move to it."
   (interactive)
   (split-window-right)
   (evil-window-right 1))
+
+(defun $evil-scroll-down-keep-pos ()
+  (interactive)
+  (evil-scroll-line-down 1)
+  (evil-next-visual-line))
+
+(defun $evil-scroll-up-keep-pos ()
+  (interactive)
+  (evil-scroll-line-up 1)
+  (evil-previous-visual-line))
 
 ;; evil bindings for occur mode
 (add-hook 'occur-mode-hook
@@ -76,7 +86,7 @@
     (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
     (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
     (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
-    (define-key evil-normal-state-map (kbd "*") '$star-keep-position)
+    (define-key evil-normal-state-map (kbd "*") '$evil-star-keep-position)
     (define-key evil-normal-state-map (kbd "]b") 'evil-next-buffer)
     (define-key evil-normal-state-map (kbd "[b") 'evil-prev-buffer)
     (define-key evil-normal-state-map (kbd "\\\\") 'imenu)
@@ -86,6 +96,8 @@
     (define-key evil-normal-state-map (kbd "\\pb") 'ivy-switch-buffer)
     (define-key evil-normal-state-map (kbd "_f") $show-full-file-path)
     (define-key evil-normal-state-map (kbd "SPC") 'counsel-grep)
+    (define-key evil-normal-state-map (kbd "C-j") '$evil-scroll-down-keep-pos)
+    (define-key evil-normal-state-map (kbd "C-k") '$evil-scroll-up-keep-pos)
     (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
     (define-key evil-visual-state-map (kbd "j") 'evil-next-visual-line)
     (define-key evil-visual-state-map (kbd "k") 'evil-previous-visual-line)
@@ -98,8 +110,8 @@
     (evil-ex-define-cmd "Q" 'evil-quit)
     (evil-ex-define-cmd "E" 'evil-edit)
     (evil-ex-define-cmd "W" 'evil-write)
-    (evil-ex-define-cmd "vs" '$split-right-and-move)
-    (evil-ex-define-cmd "Vs" '$split-right-and-move)
+    (evil-ex-define-cmd "vs" '$evil-split-right-and-move)
+    (evil-ex-define-cmd "Vs" '$evil-split-right-and-move)
     (define-key universal-argument-map (kbd "|") 'universal-argument-more)
     (define-key universal-argument-map (kbd "C-u") nil)))
  

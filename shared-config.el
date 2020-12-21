db0971a0fd50975fd8762605470e88b7a8c0bc2e
scwfri <scwfri@gmail.com>
2020-12-20 18:33:25 -0600

    update config files

--- shared-config.el
+++ shared-config.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(slime-repl-inputed-output-face ((t (:foreground "yellow")))))
+ )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.

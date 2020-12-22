ac2c97cb9213bc84d52219f9899d05216acbf6c1
scwfri <1473181+scwfri@users.noreply.github.com>
2020-12-22 12:37:22 -0600

    Update modeline.el

--- lisp/scwfri/modeline.el
+++ lisp/scwfri/modeline.el
@@ -6,33 +6,31 @@
 ;;; Code:
 
 (defun simple-mode-line-render (left right)
-  "Return a string of `window-width' length containing LEFT, and RIGHT
- aligned respectively."
-  (let* ((available-width (- (window-width) (length left) 2)))
-    (format (format " %%s %%%ds " available-width) left right)))
+  "Return a string of `window-width' length.  Containing LEFT, and RIGHT aligned respectively."
+  (let ((available-width
+         (- (window-total-width)
+            (+ (length (format-mode-line left))
+               (length (format-mode-line right))))))
+    (append left
+            (list (format (format "%%%ds" available-width) ""))
+            right)))
 
-;(setq mode-line-format
-;      '((:eval (simple-mode-line-render
-;                ;; left
-;                (format-mode-line "%b [%m] [%*]")
-;                ;; right
-;                (format-mode-line "Line: %l/%i Column: %c")))))
-
-;; mode line
 (setq-default mode-line-format
-              (list
-               ;; file status info
-               mode-line-mule-info
-               mode-line-modified
-               mode-line-frame-identification
-               ;; current buffer name
-               "%b "
-               ;; current git branch
-               '(vc-mode vc-mode)
-               ;; mode-name
-               " [%m] "
-               ;; current line and column number
-               "(%l:%c %P)"))
+              '((:eval (simple-mode-line-render
+                        ;; Left.
+                        (quote ("%e "
+                                evil-mode-line-format
+                                evil-mode-line-tag
+                                mode-line-mule-info
+                                mode-line-modified
+                                " %b -"
+                                vc-mode))
+                        ;; Right.
+                        (quote (
+                                "[%m] (%l/%i:%c %p)"
+                                mode-line-frame-identification
+                                mode-line-modes
+                                mode-line-misc-info))))))
 
 (provide 'modeline)
 ;;; modeline.el ends here

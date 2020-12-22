db70e5fe65a0a859fb370f09b7159cb7dbeebcdf
scwfri <scwfri@gmail.com>
2020-12-21 19:23:15 -0600

    add some themes

--- shared-config.el
+++ shared-config.el
@@ -11,6 +11,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("77cdb6c4d2bd17e707943d19d58759565dd14eb881f46b4031b382fc0c9ebb0a" default)))
+   '("4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "e3b2bad7b781a968692759ad12cb6552bc39d7057762eefaf168dbe604ce3a4b" default)))
 
 (provide 'shared-config)

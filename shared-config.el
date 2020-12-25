f704c25bcc7633305e6114b98187fe7200c0b3bc
scwfri <scwfri@gmail.com>
2020-12-24 19:56:01 -0600

    update elpy to work

--- shared-config.el
+++ shared-config.el
@@ -10,7 +10,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "e3b2bad7b781a968692759ad12cb6552bc39d7057762eefaf168dbe604ce3a4b" default)))
+ )
 
 (provide 'shared-config)

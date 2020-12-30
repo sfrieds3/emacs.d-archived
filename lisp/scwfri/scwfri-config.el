9da59a4781ac6e3fcc3bfe0ebe21750fe88dba88
scwfri <1473181+scwfri@users.noreply.github.com>
2020-12-29 20:49:01 -0600

    Update scwfri-config.el

--- lisp/scwfri/scwfri-config.el
+++ lisp/scwfri/scwfri-config.el
@@ -1,9 +1,18 @@
-;;;; package --- summary
+;;;; scwfri-config --- summary
 
 ;;; Commentary:
 ;;;     scwfri-settings
 
 ;;; Code:
+
+;;; display-time-world command
+(setq display-time-world-list
+  '(("America/Los_Angeles" "Seattle")
+    ("America/New_York" "New York")
+    ("Europe/Athens" "Athens")
+    ("Pacific/Auckland" "Auckland")
+    ("Asia/Shanghai" "Shanghai")))
+(setq display-time-world-time-format "%a, %d %b %I:%M %p %Z")
 
 (provide 'scwfri-config)
 ;;; scwfri-settings.el ends here

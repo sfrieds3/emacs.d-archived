0ea726e3842b1ae73ca17e7294c859f5d1989c77
scwfri <scwfri@gmail.com>
2021-02-24 17:08:44 -0600

    more optimizations of init file

--- early-init.el
+++ early-init.el
@@ -6,8 +6,21 @@
 
 ;;; code:
 
+(setq inhibit-startup-screen t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
 (setq package-enable-at-startup nil)
 (setq frame-inhibit-implied-resize t)
+
+;;; try not to gc during emacs startup... set to 10MB from 800kb
+(setq gc-cons-threshold 100000000)
+(defun $restore-gc-cons-threshold ()
+  "Restore \"gc-cons-threshold\" to 800kb.  To be added to after-init-hook."
+  (setq gc-cons-threshold 800000)
+  (message "gc-cons-threshold restored to %S"
+           gc-cons-threshold))
+(add-hook 'after-init-hook #'$restore-gc-cons-threshold)
 
 (provide 'early-init)
 ;;; early-init.el ends here

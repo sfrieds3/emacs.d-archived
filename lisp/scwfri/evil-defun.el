55706cffa613a0f5655625ca28d705ebf4b9dbbd
scwfri <1473181+scwfri@users.noreply.github.com>
2021-03-02 14:42:54 -0600

    Update evil-defun.el

--- lisp/scwfri/evil-defun.el
+++ lisp/scwfri/evil-defun.el
@@ -5,21 +5,21 @@
 
 ;;; Code:
 
-  ;;;###autoload
+;;;###autoload
 (defun $evil-clear-highlights ()
   "Clear highlight from evil-search."
   (interactive)
   (evil-ex "nohl")
   (exit-minibuffer))
 
-  ;;;###autoload
+;;;###autoload
 (defun $evil-star-keep-position ()
   "Keep position when searching."
   (interactive)
   (evil-search-word-forward)
   (evil-search-previous))
 
-  ;;;###autoload
+;;;###autoload
 (defun $evil-visualstar-keep-position ()
   "Keep current position on visual star search."
   (interactive)
@@ -28,27 +28,29 @@
     (evi-search-word-backward)
     (cua-cancel)))
 
-  ;;;###autoload
+;;;###autoload
 (defun $evil-set-jump-args (&rest ns)
   "Preserve jump list with dumb-jump.  NS args."
   (evil-set-jump))
 (advice-add 'dumb-jump-goto-file-line :before #'$evil-set-jump-args)
 
-  ;;;###autoload
+;;;###autoload
 (defun $evil-split-right-and-move ()
   "Split window to the right and move to it."
   (interactive)
   (split-window-right)
   (evil-window-right 1))
 
-  ;;;###autoload
+;;;###autoload
 (defun $evil-scroll-down-keep-pos ()
+  "Scroll down and keep current position in window."
   (interactive)
   (evil-scroll-line-down 1)
   (evil-next-visual-line))
 
-  ;;;###autoload
+;;;###autoload
 (defun $evil-scroll-up-keep-pos ()
+  "Scroll up and keep current position in window."
   (interactive)
   (evil-scroll-line-up 1)
   (evil-previous-visual-line))

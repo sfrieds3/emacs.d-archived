0fe5c1465e982a8c0c2190ceba6e1f16e1761276
scwfri <1473181+scwfri@users.noreply.github.com>
2021-03-16 10:50:49 -0500

    Update theme-config.el

--- lisp/scwfri/theme-config.el
+++ lisp/scwfri/theme-config.el
@@ -5,16 +5,20 @@
 
 ;;; Code:
 
-;;; set default preferred fonts
+;;; default preferred font-sizes
 (defvar $default-font-size
   (cond ((string= (system-name) "mixolydian") "10")
         ((string= (system-name) "phrygian") "14")
         (t "13")))
 
+;;; list of preferred fonts
 (defvar $preferred-font
   '("Iosevka Fixed SS14"
-    "DejaVu Sans Mono"))
+    "DejaVu Sans Mono"
+    "Noto Mono"
+    "JetBrains Mono"))
 
+;;; current font name.. set by $set-current-font
 (defvar $current-font nil)
 
 ;;;###autoload
